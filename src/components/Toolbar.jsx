472290e3fa4ce4a94277cd7d0de6bc9456fb3293
FifthLord <nborysenko777@gmail.com>
2022-10-27 21:07:49 +0300

    add line logic

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -5,6 +5,7 @@ import "../style/toolbar.scss"
 import Brush from '../tools/Brush';
 import Circle from '../tools/Circle';
 import Eraser from '../tools/Eraser';
+import Line from '../tools/Line';
 import Rect from '../tools/Rect';
 
 const Toolbar = () => {
@@ -14,7 +15,7 @@ const Toolbar = () => {
          <button className='toolbar__bth rect' onClick={() => toolState.setTool(new Rect(canvasState.canvas))} />
          <button className='toolbar__bth circle' onClick={() => toolState.setTool(new Circle(canvasState.canvas))} />
          <button className='toolbar__bth eraser' onClick={() => toolState.setTool(new Eraser(canvasState.canvas))} />
-         <button className='toolbar__bth line' />
+         <button className='toolbar__bth line' onClick={() => toolState.setTool(new Line(canvasState.canvas))} />
          <input style={{ marginLeft: 10 }} type="color" />
          <button className='toolbar__bth undo' />
          <button className='toolbar__bth redo' />
