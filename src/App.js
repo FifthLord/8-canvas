105418495afa705b31db838aeaef111ee419f2b5
FifthLord <nborysenko777@gmail.com>
2022-11-02 23:50:37 +0200

    add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import SettingBar from './components/SettingBar';
 import Canvas from "./components/Canvas"
 import Toolbar from './components/Toolbar';
 import './style/app.scss';
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
    return (
+      //* */ BrowserRouter in index.js
+      // <div className="app">
+      //    <Routes>
+      //       <Route path='/:id' element={<Toolbar />} />
+      //       <Route path='/:id' element={<SettingBar />} />
+      //       <Route path='/:id' element={<Canvas />} />
+      //       <Navigate to={`f${(+new Date).toString(16)}`} />
+      //    </Routes>
+      // </div>
       <div className="app">
          <Toolbar />
          <SettingBar />
          <Canvas />
+         <Navigate to={`f${(+new Date).toString(16)}`} />
       </div>
    );
 }
