d81165f7aa25030c35468c10150d6038a279116c
FifthLord <nborysenko777@gmail.com>
2022-11-07 18:07:10 +0200

    add update page logic

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const express = require('express')
 const app = express()
 const WSServer = require('express-ws')(app)
 const aWss = WSServer.getWss()
-
+const cors = require('cors')
 const PORT = process.env.PORT || 3000
+const fs = require('fs')
+const path = require('path')
+
+app.use(cors())
+app.use(express.json())
 
 app.ws('/', (ws, req) => {
-   // console.log('Підключеня виконано')
-   // ws.send('Ти успішно законектився')
    ws.on('message', (msg) => {
       msg = JSON.parse(msg)
       switch (msg.method) {
@@ -20,6 +23,27 @@ app.ws('/', (ws, req) => {
             break
       }
    })
+})
+
+app.post('/image', (req, res) => {
+   try {
+      const data = req.body.img.replace('data:image/png;base64,', '')
+      fs.writeFileSync(path.resolve(__dirname, 'files', `${req.query.id}.jpg`), data, 'base64')
+      return res.status(200).json({ message: "Завантажено" })
+   } catch (error) {
+      console.log(error);
+      return res.status(500).json('error')
+   }
+})
+app.get('/image', (req, res) => {
+   try {
+      const file = fs.readFileSync(path.resolve(__dirname, 'files', `${req.query.id}.jpg`))
+      const data = 'data:image/png;base64,' + file.toString('base64')
+      res.json(data)
+   } catch (error) {
+      console.log(error);
+      return res.status(500).json('error')
+   }
 })
 
 app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
